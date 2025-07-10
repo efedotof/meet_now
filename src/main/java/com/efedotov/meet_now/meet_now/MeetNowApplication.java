193b73440da80325ded11dc72982ddb24597cb9d
Efedotov <fedmehol@icloud.com>
2025-07-10 14:26:45 +0300

    create project

---
+++ src/main/java/com/efedotov/meet_now/meet_now/MeetNowApplication.java
@@ -0,0 +1,13 @@
+package com.efedotov.meet_now.meet_now;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetNowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/efedotov/meet_now/meet_now/MeetNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efedotov.meet_now.meet_now;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
