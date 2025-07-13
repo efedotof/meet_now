ff924429446152f663a4a860c96fbd06ce3e55f8
Efedotov <fedmehol@icloud.com>
2025-07-13 12:31:18 +0300

    create test ui

--- lib/features/main_home/view/main_home_screen.dart
+++ lib/features/main_home/view/main_home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:auto_route/auto_route.dart';
+import 'package:meet_now_app/route/app_route.dart';
 
 @RoutePage()
 class MainHomeScreen extends StatelessWidget {
@@ -7,6 +8,43 @@ class MainHomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return AutoTabsRouter(
+      // list of your tab routes
+      // routes used here must be declared as children
+      // routes of /dashboard
+      routes: const [
+        SearchRoute(),
+        ChatRoute(),
+        FriendsRoute(),
+        SettingsRoute(),
+      ],
+      transitionBuilder:
+          (context, child, animation) =>
+              FadeTransition(opacity: animation, child: child),
+      builder: (context, child) {
+        final tabsRouter = AutoTabsRouter.of(context);
+        return Scaffold(
+          body: child,
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: tabsRouter.activeIndex,
+            onTap: (index) {
+              tabsRouter.setActiveIndex(index);
+            },
+            items: [
+              BottomNavigationBarItem(
+                label: 'Search',
+                icon: Icon(Icons.search),
+              ),
+              BottomNavigationBarItem(label: 'Chat', icon: Icon(Icons.message)),
+              BottomNavigationBarItem(label: 'Friend', icon: Icon(Icons.group)),
+              BottomNavigationBarItem(
+                label: 'Setting',
+                icon: Icon(Icons.settings),
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
